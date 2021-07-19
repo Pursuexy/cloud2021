df354e4af0d4c6fa25398efda9fea3ba571ff221
Pursuexy <1365409209@qq.com>
2021-07-19 11:45:30 +0800

    第一季SpringCloud完成

--- cloud-consumer-order80/src/main/java/com/pursuexy/cloud2021/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/pursuexy/cloud2021/controller/OrderController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 import javax.annotation.Resource;
 import java.net.URI;
 import java.util.List;
@@ -23,7 +24,7 @@ import java.util.List;
 @Slf4j
 public class OrderController {
 
-//    public static final String PAYMENT_URL = "http://localhost:8001";
+    //    public static final String PAYMENT_URL = "http://localhost:8001";
     public static final String PAYMENT_URL = "http://CLOUD-PROVIDER-PAYMENT";
 
     @Resource
@@ -42,7 +43,7 @@ public class OrderController {
 
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Integer id) {
-        return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id, CommonResult.class);
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
     }
 
     @GetMapping("/consumer/payment/getForEntity/{id}")
@@ -50,19 +51,27 @@ public class OrderController {
         ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
         if (entity.getStatusCode().is2xxSuccessful()) {
             return entity.getBody();
-        }else{
-            return new CommonResult<>(444,"操作失败");
+        } else {
+            return new CommonResult<>(444, "操作失败");
         }
     }
 
     @GetMapping("/consumer/payment/lb")
     public String getPaymentLB() {
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PROVIDER-PAYMENT");
-        if (instances==null || instances.size()<=0) {
+        if (instances == null || instances.size() <= 0) {
             return null;
         }
         ServiceInstance instance = loadBalancer.instances(instances);
         URI uri = instance.getUri();
-        return restTemplate.getForObject(uri+"/payment/lb", String.class);
+        return restTemplate.getForObject(uri + "/payment/lb", String.class);
     }
+
+    // ====================> zipkin+sleuth
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipkin() {
+        String result = restTemplate.getForObject("http://localhost:8001" + "/payment/zipkin/", String.class);
+        return result;
+    }
+
 }
